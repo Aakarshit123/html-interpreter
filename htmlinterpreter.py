cf41bfb33bf064cf6c911d7a5f98688630a79c56
Aakarshit123 <Aakarshit.cse@gmail.com>
2025-07-08 11:53:20 +0530

    Add files via upload

---
+++ htmlinterpreter.py
@@ -0,0 +1,13 @@
+# html_interpreter.py
+import streamlit as st
+
+st.set_page_config(page_title="HTML Interpreter", layout="centered")
+st.title("🧾 HTML Live Interpreter")
+
+html_code = st.text_area("✍️ Write HTML code below", height=300, placeholder="<h1>Hello World!</h1>")
+st.markdown("### Live Preview")
+
+if html_code:
+    st.components.v1.html(html_code, height=400, scrolling=True)
+else:
+    st.info("🖋️ Your live output will appear here as you type.")
