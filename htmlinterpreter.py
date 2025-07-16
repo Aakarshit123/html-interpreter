6596b659bdb06ebbcdab20fd2faec6804f857d2c
Aakarshit123 <Aakarshit.cse@gmail.com>
2025-07-16 18:53:28 +0530

    Update htmlinterpreter.py

--- htmlinterpreter.py
+++ htmlinterpreter.py
@@ -2,12 +2,12 @@
 import streamlit as st
 
 st.set_page_config(page_title="HTML Interpreter", layout="centered")
-st.title("🧾 HTML Live Interpreter")
+st.title(" HTML Live Interpreter")
 
-html_code = st.text_area("✍️ Write HTML code below", height=300, placeholder="<h1>Hello World!</h1>")
+html_code = st.text_area("✍ Write HTML code below", height=300, placeholder="<h1>Hello World!</h1>")
 st.markdown("### Live Preview")
 
 if html_code:
     st.components.v1.html(html_code, height=400, scrolling=True)
 else:
-    st.info("🖋️ Your live output will appear here as you type.")
+    st.info("🖋 Your live output will appear here as you type.")
